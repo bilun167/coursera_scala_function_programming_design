d92821d0617d90f0b31758a413891237b28d525a
Tai Huynh <bilun167@gmail.com>
2016-07-06 16:06:39 +0800

    Impl w1 topics.

---
+++ w1/JSON.scala
@@ -0,0 +1,37 @@
+/**
+  * Created by taihuynh on 24/6/16.
+  */
+abstract class JSON {
+	def show(json: JSON): String = json match {
+		case JSeq(elems) => "[" + (elems map show mkString ", ") + "]"
+		case JObj(bindings) =>
+			val assocs = bindings map {
+				case (key, value) => "\"" + key + "\": " + show(value)
+			}
+			"{" + (assocs mkString ", ") + "}"
+		case JNum(num) => num.toString
+		case JStr(str) => '\"' + str + '\"'
+		case JBool(b) => b.toString
+		case JNull => "null"
+	}
+}
+
+case class JSeq (elems: List[JSON]) extends JSON {
+
+}
+case class JObj (bindings: Map[String, JSON]) extends JSON {
+
+}
+case class JNum (num: Double) extends JSON {
+
+}
+case class JStr (str: String) extends JSON {
+
+}
+case class JBool(b: Boolean) extends JSON {
+
+}
+case object JNull extends JSON {
+
+}
+
